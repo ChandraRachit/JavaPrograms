8fc5d0ff11edbf9fbe2686804af60cbdb03b59f7
Rachit Chandra <rachitchandra220@gmail.com>
2020-11-06 23:36:22 +0530

    added sorting program

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,16 +1,6 @@
 package sorting;
 
-/**
- * Selection Sort
- * Time Complexity: O(n2) as there are two nested loops.
- * Auxiliary Space: O(1)
- * It never makes more than O(n) swaps and can be useful, 
- * when memory write is a costly operation.
- * @author Rachit Chandra
- *
- */
-public class SelectionSort {
-
+public class InsertionSort {
 	public static void main(String args[]) {
 		//Array to be sorted
 		int[] arr = { 5,4,3,2,1,9,1 };
@@ -24,7 +14,7 @@ public class SelectionSort {
 		System.out.println("]");
 
 		//Calling sorting method to sort the array
-		selectionSort(arr);
+		insertionSort(arr);
 
 		//Printing array after selection sort
 		System.out.println("Elements after selection sorting");
@@ -35,19 +25,21 @@ public class SelectionSort {
 		System.out.println("]");
 	}
 
-	//Method for selection sort
-	public static void selectionSort(int arr[]) {
-		for(int i=0;i<arr.length-1;i++) { 
-			int min=i;
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[j]<arr[min]) {
-					min=j;
-				}
+	//Method for insertion sort
+	public static void insertionSort(int arr[]) {
+ 		for (int i = 1; i < arr.length; ++i) { 
+			int key = arr[i]; 
+			int j = i - 1; 
+
+			/* Move elements of arr[0..i-1], that are 
+	               greater than key, to one position ahead 
+	               of their current position */
+			while (j >= 0 && arr[j] > key) { 
+				arr[j + 1] = arr[j]; 
+				j = j - 1; 
 			} 
-			int temp= arr[i];
-			arr[i]=arr[min];
-			arr[min]=temp;
-		}
+			arr[j + 1] = key; 
+		} 
 	}
 
 }
